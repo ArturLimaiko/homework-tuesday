816428959ba4b342b30e4cb85e373fd5b4d02a24
Artur <alimayko91@gmail.com>
2024-05-10 14:05:33 +0300

    HW#4 complete onChangeCallback in SuperCheckbox component

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -9,6 +9,10 @@ import s from './SuperCheckbox.module.css'
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement>
 
+
+// Omit это вспомогательный тип TypeScript, который предоставляется стандартной библиотекой TypeScript
+// и позволяет исключить определенные свойства из типа объекта.
+// в нашем SuperCheckboxPropsType убираем тип 'type' а все остальное оставляеи и добавляем несколько своих свойств
 type SuperCheckboxPropsType = Omit<DefaultInputPropsType, 'type'> & {
     onChangeChecked?: (checked: boolean) => void
     spanClassName?: string
@@ -26,10 +30,10 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps
     }
 ) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // задачка на написание онченджа
-
-    }
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => // задачка на написание онченджа
+        onChangeChecked
+            ? onChangeChecked(e.currentTarget.checked)
+            : null
 
     const finalInputClassName = s.checkbox
         + (className ? ' ' + className : '')
