19b608aa6c46f5e2def817b97b48947fe97c193c
Artur <alimayko91@gmail.com>
2024-05-10 13:33:42 +0300

    HW#4 complete SuperButton component( доделал задачку с передачей стилей через тернарный ? : )

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,22 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    // const finalClassName = s.button
+    //     + (disabled
+    //             ? true
+    //             : xType === 'red'
+    //                 ?...
+    //     + (className ? ' ' + className : '') // задачка на смешивание классов
+
+    const finalClassName = `${s.button}
+        ${disabled ? s.disabled : xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}
+        ${className ? className : ''}`;
+    // 1 -вначале добавляем базовый класс стиля кнопки ${s.button}
+    // 2 - в этой строке мы решаем, какой стиль добавить в зависимости от значений disabled и xType. Если кнопка отключена (disabled === true), мы добавляем класс s.disabled.
+    // В противном случае, мы проверяем xType: если xType равен 'red', мы добавляем класс s.red,
+    // если xType равен 'secondary', мы добавляем класс s.secondary, иначе добавляем класс s.default.
+    // 3 - Здесь мы проверяем переданный в компонент SuperButton класс className. Если он существует, мы добавляем его к списку классов в finalClassName.
+
 
     return (
         <button
