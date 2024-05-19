12c11c0ee6c7253225b8d81d6a044cc4c93853c2
Artur <alimayko91@gmail.com>
2024-05-19 13:28:05 +0300

    HW#5 begin...

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,18 +18,37 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                <Route path={'/'}  element={<Navigate to={'/pre-junior'}/> } />
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path={'/pre-junior'}  element={<PreJunior/>} />
+                <Route path={'/junior'}  element={<Junior/>} />
+                <Route path={'/junior-plus'}  element={<JuniorPlus/>} />
+
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path={'/page/error404'}  element={<Error404/>} />
+                <Route path={'/*'} element={ <Navigate to={'/page/error404'}/> }/>
             </Routes>
         </div>
     )
 }
 
 export default Pages
+
+
+{/*
+Работаем с роутингами
+
+1. yarn app react-router-dom
+
+2. оборачиваем все в BrowserRouter ( с github pages корректнее работает HashRouter
+
+3. Подключаем страницы в Pages
+4. Прописываем <Routes>(оборачиваем) и внутрь кладем  <Route/>
+5. В каждую Route вставляем нужную страницу  <Route path={''}  element={<PreJunior/>} />  как пример
+в path={''} прописываем путь к страницам( в принципе можно указать любой) , element={ } - содержат страницы
+6. подключаем на страницу <Navigate/> внутри element и прописываем to={'/error404'} и нашу страницу с ошибкой
+7. Navlink тег, используется для создания навигационной ссылки в React приложениях, которые используют маршрутизацию
+ Он позволяет создавать ссылки с активным состоянием, когда текущий маршрут соответствует URL ссылки.
+*/}
